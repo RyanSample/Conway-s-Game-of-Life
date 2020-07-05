ba251dedc7f991c3304be1b5a1403bb73f7d1666
RyanSample <sampleryan1@gmail.com>
2020-07-05 18:25:18 -0500

    Removed depracated code from cell.py

--- models/cell.py
+++ models/cell.py
@@ -8,18 +8,6 @@ class Cell():
         """Return a boolean value indicating whether the cell is living or not"""
         return self.living
 
-    def decrement_neighbors(self):
-        """Deprecated"""
-        self.number_neighbors += 1
-
-    def increment_neigbors(self):
-        """Deprecated"""
-        self.number_neighbors -= 1
-
-    def get_number_neighbors(self):
-        """Deprecated"""
-        return self.number_neighbors
-
     def toggle_living(self):
         """Toggles the cells status as living"""
         self.living = not self.living
